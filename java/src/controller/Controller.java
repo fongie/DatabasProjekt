858b588f632ab78219c481d419ba2631fe0232d3
Anders <andpet52@kth.se>
2017-12-01 13:07:36 +0100

    new class Command update Controller

---
+++ java/src/controller/Controller.java
@@ -0,0 +1,28 @@
+package controller;
+
+/**
+ * Created by Anders on 2017-12-01.
+ */
+public class Controller {
+
+    /**
+     * Show all products
+     */
+    public void showAllProducts() {
+
+    }
+
+    /**
+     * Show all stores that doesn't have a specific product in stock
+     */
+    public void notInStock(){
+
+    }
+
+    /**
+     * Add a new product to your watchlist
+     */
+    public void watchProduct(){
+
+    }
+}
--- java/src/view/Command.java
+++ java/src/view/Command.java
@@ -8,8 +8,8 @@ public class Command {
     // lists available commands
     public void showCommandList() {
         System.out.println("\nCommand list\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n" +
-                "product    = Show all products\n" +
-                "stock      = Show all stores that do NOT have a product in stock.\n" +
-                "watch      = Add a product to your watch list\n");
+                "product    => Show all products\n" +
+                "stock      => Show all stores that doesn't have a specific product in stock\n" +
+                "watch      => Add a product to your watch list\n");
     }
 }
